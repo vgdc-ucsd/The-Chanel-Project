08a352d0f6aed7922f05376cad8253e234551ac7
Ryan Luutuyen <rjluutuyenc@gmail.com>
2024-02-22 03:19:27 -0800

    Procedural generation
    Can be procedurally generated but it creates multiple nodes in the same spot; need to fix

--- Assets/Scripts/GenerateMap.cs
+++ Assets/Scripts/GenerateMap.cs
@@ -1,29 +1,105 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
+using static TreeEditor.TreeEditorHelper;
 
 
 public class GenerateMap : MonoBehaviour
 {
     public GameObject Encounter;
+    public GameObject Shop;
+    public GameObject Event;
     public GameObject start;
-    public LineRenderer line;
-    public Vector3 startPos;
+    public Vector3 Pos;
+    public List<Vector3> positions = new();
+    public List<Vector3> newPositions = new();
     // Start is called before the first frame update
     void Start()
     {
-        //sets start location based on starting node/area to generate the map
-        startPos = new Vector3(start.transform.position.x, start.transform.position.y);
+        positions = new List<Vector3> { new(start.transform.position.x, start.transform.position.y) };
         transform.rotation = Quaternion.identity;
-        
-        Instantiate(Encounter, new Vector3(startPos.x + 100, startPos.y),transform.rotation, Encounter.transform.parent);
-        line.positionCount = 2;
+        GenerateNodes(positions);
     }
 
     // Update is called once per frame
     void Update()
     {
-        line.SetPosition(0,startPos);
-        line.SetPosition(1,new Vector3(startPos.x+100, startPos.y));
+
+    }
+
+    void GenerateNodes(List<Vector3> currentList)
+    {
+        for(int h = 0; h < 4; h++)
+        {
+            //creates new nodes/events
+            for (int i = 0; i < currentList.Count; i++)
+            {
+                Pos = currentList[i];
+                //nodes can only be created on upper two sides and right side of hexagon; (not sure the limit or direction of map yet)
+                for (int j = 0; j < 3; j++)
+                {
+                    int eventType;
+                    int directionNum;
+                    GameObject clone = null;
+                    GameObject nodeType;
+                    List<Vector3> directions = new() { new(Pos.x + 100, Pos.y), new(Pos.x + 50, (float)(Pos.y + 86.6)), new(Pos.x - 50, (float)(Pos.y + 86.6)) };
+                    newPositions = new();
+                    //ensures at least one path from a node exists
+                    if (newPositions.Count == 0)
+                    {
+                        eventType = Random.Range(1, 90);
+                    }
+                    else
+                    {
+                        eventType = Random.Range(1, 100);
+                    }
+                    //50% chance to generate an encounter(enemy event)
+                    if (eventType <= 50)
+                    {
+                        nodeType = Encounter;
+                    }
+                    //30% chance to generate an event
+                    else if (eventType <= 80)
+                    {
+                        nodeType = Event;
+                    }
+                    //19% chance to generate a shop
+                    else if (eventType <= 99)
+                    {
+                        nodeType = Shop;
+                    }
+                    else
+                    {
+                        nodeType = null;
+                    }
+                    directionNum = Random.Range(0, 2);
+                    if (directionNum == 0)
+                    {
+                        //creates encounter node to the east
+                        CreateNode(nodeType, directions[directionNum], clone);
+                    }
+                    else if (directionNum == 1)
+                    {
+                        //creates encounter node to the northeast
+                        CreateNode(nodeType, directions[directionNum], clone);
+                    }
+                    else if (directionNum == 2)
+                    {
+                        //creates encounter node to the northwest
+                        CreateNode(nodeType, directions[directionNum], clone);
+                    }
+                    currentList = newPositions;
+                }
+            }
+        }
+    }
+
+    void CreateNode(GameObject type, Vector3 pos, GameObject newObject)
+    {
+        Instantiate(type, pos, transform.rotation, Encounter.transform.parent);
+        newObject = Instantiate(type, pos, transform.rotation, Encounter.transform.parent);
+        newPositions.Add(new Vector3(newObject.transform.position.x, newObject.transform.position.y));
     }
 }
+
