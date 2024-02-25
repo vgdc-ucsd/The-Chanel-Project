67139496956468e9694f3a442cbf61b00bce1610
DrFreshPotato <khovandrew@gmail.com>
2024-02-25 00:28:18 -0800

    Shop Scene, Basic UI layout

---
+++ Assets/ScriptableObjects/Scripts/Shop/ShopManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopManager : MonoBehaviour
+{
+    
+    //Adjusts the row and column dimensions of the Grid
+    //Might need to change later to split both the cards and relics
+    public int rowSize;
+    public int colSize;
+
+
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/ScriptableObjects/Scripts/Shop/Upgrade.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// TEMP
+using UnityEngine.SceneManagement;
+
+public class Upgrade : MonoBehaviour
+{
+
+    public void upgradeCard()
+    {
+        Debug.Log("TODO: OPEN DECKLIST TO UPGRADE");
+ 
+    }
+}
