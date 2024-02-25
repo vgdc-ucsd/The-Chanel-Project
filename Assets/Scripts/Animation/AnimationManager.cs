57f78ee020a5a7ba732a6190e79a20f17255a529
Ethan Schwartzman <ethancschwartzman@gmail.com>
2024-02-24 21:31:01 -0800

    Animation Manager
    Added a system for creating and queueing animations

---
+++ Assets/Scripts/Animation/AnimationManager.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationManager : MonoBehaviour
+{
+    private Queue<QueueableAnimation> animations = new Queue<QueueableAnimation>();
+    private bool activelyPlaying = false;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if(!activelyPlaying && animations.Count > 0) {
+            StartCoroutine(PlayAnimations());
+        }
+    }
+
+    // Plays the animation immediately
+    public void Play(IEnumerator animation) {
+        StartCoroutine(animation);
+    }
+
+    // Plays the animation when the delay from the previous animation has finished
+    public void QueueAnimation(QueueableAnimation qa) {
+        animations.Enqueue(qa);
+    }
+
+    private IEnumerator PlayAnimations() {
+        activelyPlaying = true;
+        
+        // While the animation queue is not empty
+        while(animations.Count > 0) {
+            float startTime = Time.time;
+            // Dequeues and plays the animation
+            QueueableAnimation current = animations.Dequeue();
+            Play(current.Animation);
+
+            // Waits until the delay is done
+            while(Time.time - startTime < current.Delay) {
+                yield return null;
+            }
+        }
+        activelyPlaying = false;
+    }
+
+    // Translates the transform at origin to the position at dest
+    public IEnumerator SimpleTranslate(Transform origin, Vector3 dest, float duration, InterpolationMode mode) {
+        float startTime = Time.time;
+        Vector3 startPos = origin.position;
+        float elapsedTime = Time.time - startTime;
+
+        // Interpolates between two positions until elapsedTime reaches duration
+        while(elapsedTime < duration) {
+            float t = elapsedTime / duration;
+            origin.position = Interpolation.Interpolate(startPos, dest, t, mode);
+            elapsedTime = Time.time - startTime;
+            yield return null;
+        }
+
+        origin.position = dest;
+    }
+}
