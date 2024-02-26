df88fee7a6d07e6a6dd8ca077509cd6abfee2a16
DrFreshPotato <khovandrew@gmail.com>
2024-02-25 19:32:56 -0800

    GridLayOut, Simple Prefab

--- Assets/ScriptableObjects/Scripts/Shop/ShopManager.cs
+++ Assets/ScriptableObjects/Scripts/Shop/ShopManager.cs
@@ -1,16 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShopManager : MonoBehaviour
 {
     
     //Adjusts the row and column dimensions of the Grid
     //Might need to change later to split both the cards and relics
-    public int rowSize;
-    public int colSize;
+    public int rowSize = 2;
+    public int colSize = 5;
 
-
+    //public List<GameObject>() CardSlots = new List<>(); 
     
     
     
