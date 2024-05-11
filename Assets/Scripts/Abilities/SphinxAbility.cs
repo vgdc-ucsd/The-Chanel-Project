a6cf1607a8192cbc2f97e05e2153baa4f3b81857
Dubshott <abhideevi@gmail.com>
2024-05-11 12:02:04 -0700

    Added Sphinx

---
+++ Assets/Scripts/Abilities/SphinxAbility.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Ability Description:
+// This card can only die if you hit it with the exact amount of damage as health. 
+[CreateAssetMenu(menuName = "Abilities/SphinxAbility")]
+public class SphinxAbility : Ability
+{
+    public override ActivationCondition Condition { get { return ActivationCondition.OnReceiveDamage; } }
+
+    public override void Activate(UnitCard c, ActivationInfo info)
+    {
+        if (info.OverkillDamage > 0)
+        {
+            c.Health = 1;
+        }
+
+        AnimationManager.Instance.UpdateCardInfoAnimation(info.Duel, c);
+
+    }
+}
