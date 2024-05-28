2e24f4bfc601ba76436484601f473c16403304ff
aodintsov1 <aodintsov4edu@gmail.com>
2024-05-27 21:28:31 -0700

    Combat Branch merge

--- Assets/Scripts/Abilities/LancelotAbility.cs
+++ Assets/Scripts/Abilities/LancelotAbility.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 // Abilty Description:
@@ -9,12 +10,40 @@ public class LancelotAbility : Ability
 
     public override void Activate(UnitCard c, ActivationInfo Info)
     {
+        Vector2Int forward;
+        //Vector2Int doubleforward;
+
+        if (c.CurrentTeam == Team.Player)
+        {
+            forward = new Vector2Int(0, 1);
+            //doubleforward = new Vector2Int(0, 2);
+        }
+        else
+        {
+            forward = new Vector2Int(0, -1);
+            //doubleforward = new Vector2Int(0, -2);
+        }
         if (Info.OverkillDamage > 0)
         {
+            UnitCard middleCard = Info.Duel.DuelBoard.GetFrontCard(Info.TargetCard.Pos, c.CurrentTeam);
+            Debug.Log(middleCard);
+            if (middleCard == null)
+            {
+                return;
+            }//board.getpositiontargetcard then add forward direction to it
+            UnitCard backCard = Info.Duel.DuelBoard.GetFrontCard(middleCard.Pos, c.CurrentTeam);
+            Debug.Log(backCard);
             /*
-            if(UnitCard card = )
+            BoardCoords doubleForwardPos = Info.Duel.DuelBoard.GetFrontTile(middleCard.Pos, c.CurrentTeam);
+            UnitCard backCard = Info.Duel.DuelBoard.GetCard(doubleForwardPos);
             */
+            if (backCard != null)
+            {
+                Info.Duel.DealDamage(backCard, Info.OverkillDamage);
+            }
+
         }
+
         AnimationManager.Instance.UpdateCardInfoAnimation(Info.Duel, c);
     }
 }
