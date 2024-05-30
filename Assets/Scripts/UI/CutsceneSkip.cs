1661942759eec3879ba120367445c24ea0b2dd72
Ethan Schwartzman <ethancschwartzman@gmail.com>
2024-05-30 02:03:53 -0700

    Cutscene skip

---
+++ Assets/Scripts/UI/CutsceneSkip.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CutsceneSkip : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            SceneManager.LoadScene(MenuScript.DUEL_INDEX);
+        }       
+    }
+}
